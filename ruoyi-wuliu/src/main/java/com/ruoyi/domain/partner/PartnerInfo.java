3a646cbd1d9ff0bc4cb5c94095a184717eb886a4
even <986536869@qq.com>
2025-03-02 13:50:35 +0800

    增加决策承运商

--- ruoyi-wuliu/src/main/java/com/ruoyi/domain/partner/PartnerInfo.java
+++ ruoyi-wuliu/src/main/java/com/ruoyi/domain/partner/PartnerInfo.java
@@ -1,4 +1,55 @@
 package com.ruoyi.domain.partner;
 
+/**
+ * 配送商基础信息
+ */
 public class PartnerInfo {
+    /**
+     * id
+     */
+    private Long id;
+    /**
+     * 名称
+     */
+    private String name;
+    /**
+     * 编码
+     */
+    private String code;
+    /**
+     * 优先级
+     */
+    private String level;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
--- ruoyi-wuliu/src/main/java/com/ruoyi/service/partner/PartnerFactory.java
+++ ruoyi-wuliu/src/main/java/com/ruoyi/service/partner/PartnerFactory.java
@@ -1,12 +1,16 @@
 package com.ruoyi.service.partner;
 
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.domain.partner.PartnerContext;
 import com.ruoyi.domain.partner.PartnerInfo;
+import com.ruoyi.system.service.ISysConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 承运商决策
@@ -16,8 +20,13 @@ public class PartnerFactory {
     /**
      * 决策链
      */
-    List<PartnerDecision> partnerDecisionList;
+    @Autowired
+    private List<PartnerDecision> partnerDecisionList;
 
+    @Autowired
+    private ISysConfigService configService;
+
+    private static final String PARTNER_BLACK_LIST = "Delivery_Black_List";
     /**
      * 决策可用CP
      * @return
@@ -40,8 +49,13 @@ public class PartnerFactory {
         /**
          * 黑名单过滤
          */
-
-       return partnerInfos;
+        String partnerBlackList = configService.selectConfigByKey("PARTNER_BLACK_LIST");
+        if(StringUtils.isNotBlank(partnerBlackList)){
+            partnerInfos = partnerInfos.stream()
+                    .filter(partner -> !partnerBlackList.contains(partner.getCode()))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+        return partnerInfos;
     }
 
 }
